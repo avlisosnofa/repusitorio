dd2de9b62810882f6b702dd87e9f870b0ae79919
avlisosnofa <shark06922@sharkcoders.pt>
2024-10-19 19:37:12 +0100

    Add files via upload

---
+++ float input.py
@@ -0,0 +1,29 @@
+#soma com números inteiros
+
+x = 4
+y = 536279840
+resultado = x + y
+
+print(resultado)
+
+#soma com números com casas decimais
+
+a = 5.7
+b = 2.978290365812340823
+result = a + b
+
+print(result)
+
+#soma com números inteiros e com casas decimais
+
+c = 8
+d = 2.3
+res = c + d
+
+print(res)
+
+n1 = int(input("Qual o primeiro número\n"))
+n2 = float(input("Qual o segundo número\n"))
+
+mult = n1 * n2
+print(mult)
---
+++ quadrato.py
@@ -0,0 +1,8 @@
+lado = int(input("Insira o comprimento do lado do quadrado \n"))
+
+perimetro = lado * 4
+area = lado * lado
+
+print()
+print("Perímetro:",perimetro)
+print("Área:",area)
