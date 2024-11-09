f4b562f4a3de93fae17db7e256030d4b668f6a11
avlisosnofa <shark06922@sharkcoders.pt>
2024-11-09 19:57:56 +0000

    marca.py

--- marca.py
+++ marca.py
@@ -1,20 +1,20 @@
 marca = input("Qual marca de telemoble tem?\n")
 
-if marca in 'Apple':
+if marca in 'Apple' or 'apple':
     print("Péssima escolha")
-elif marca in 'Samsung':
+elif marca in 'Samsung' or 'samsung':
     print("Ok")
-elif marca in 'Xiaomi':
+elif marca in 'Xiaomi' or 'xiamoi':
     print("Boa escolha manel")
-elif marca in 'Huawei':
+elif marca in 'Huawei' or 'huawei':
     print("Tens problemas?")
-elif marca in 'Nokia':
+elif marca in 'Nokia' or 'nokia':
     print("Ganda escolha, só não deixes cair")
-elif marca in 'LG':
+elif marca in 'LG' or 'lg':
     print("Sinto muito")
-elif marca in 'OnePlus':
+elif marca in 'OnePlus' or 'oneplus':
     print("Deslcupa rico")
-elif marca in 'Alcatel':
+elif marca in 'Alcatel' or 'alcatel':
     print('Suicida-te')
 else:
     print("Não tenho")
