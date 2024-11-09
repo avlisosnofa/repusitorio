b1a708de42e1c9932718c1a6b1839010ac88a419
avlisosnofa <shark06922@sharkcoders.pt>
2024-11-09 19:50:09 +0000

    Add files via upload

---
+++ marca.py
@@ -0,0 +1,20 @@
+marca = input("Qual marca de telemoble tem?\n")
+
+if marca in 'Apple':
+    print("Péssima escolha")
+elif marca in 'Samsung':
+    print("Ok")
+elif marca in 'Xiaomi':
+    print("Boa escolha manel")
+elif marca in 'Huawei':
+    print("Tens problemas?")
+elif marca in 'Nokia':
+    print("Ganda escolha, só não deixes cair")
+elif marca in 'LG':
+    print("Sinto muito")
+elif marca in 'OnePlus':
+    print("Deslcupa rico")
+elif marca in 'Alcatel':
+    print('Suicida-te')
+else:
+    print("Não tenho")
